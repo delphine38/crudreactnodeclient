7c8f36a02051c6894fff09d7dec0e5696fe9d2d7
delphine <delphin38@live.fr>
2022-05-08 14:30:16 +0200

    data in bdd ok

--- server/index.js
+++ server/index.js
@@ -1,9 +1,38 @@
 const express = require('express')
+const bodyParser = require ('body-parser')
+const cors = require('cors')
 const app = express();
+const mysql = require('mysql')
 
-app.get('/', (req, res)=>{
-    res.send('Hello i world')
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'cruddatabase',
+    password: 'cruddatabase',
+    database: 'cruddatabase'
+});
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/api/get", (req, res)=>{
+    const sqlSelect = "SELECT * movie_reviews";
+    db.query(sqlInsert, (err, result)=>{
+        console.log(result);
+    })
 })
+
+
+app.post("/api/insert", (req, res)=>{
+
+    
+const movieName = req.body.movieName
+    const moviReview = req.body.moviReview
+    const sqlInsert = "INSERT INTO movie_reviews(movieName, moviReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, moviReview], (err, result)=>{
+        console.log(result);
+    })
+});
 
 app.listen(3001,()=>{
     console.log('running on port 3001');
--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+
+  const [movieName, setMovieName] = useState("");
+  const [review, setReview] = useState("");
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName, 
+      moviReview: review
+    }).then(()=>{
+      alert("successful insert");
+    });
+  };
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -21,19 +36,19 @@ function App() {
       <h1>CRUD APPLICATION REACT.JS / NODE.JS</h1>
       <div className="form">
         <label>Nom : </label>
-        <input type="text" name="movieName"></input>
-        {/* <input type="text" name="movieName" onChange={(e)=>{
+        {/* <input type="text" name="movieName"></input> */}
+        <input type="text" name="movieName" onChange={(e)=>{
           setMovieName(e.target.value)
-        }}></input> */}
+        }}></input>
 
         <label>Review : </label>
-        <input type="text" name="review"></input>
-        {/* <input type="text" name="review" onChange={(e)=>{
+        {/* <input type="text" name="review"></input> */}
+        <input type="text" name="review" onChange={(e)=>{
           setReview(e.target.value)
-        }}></input> */}
+        }}></input>
 
-        <button >Envoi</button>
-        {/* <button onClick={submitReview}>Envoi</button> */}
+        {/* <button >Envoi</button> */}
+        <button onClick={submitReview}>Envoi</button>
       </div>
 
     </div>
