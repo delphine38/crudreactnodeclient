a58c05575bb9cb773cdea8fd6204090d6c16bbdd
delphine <delphin38@live.fr>
2022-05-08 12:18:10 +0200

    fomulaire and css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +17,25 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>CRUD APPLICATION REACT.JS / NODE.JS</h1>
+      <div className="form">
+        <label>Nom : </label>
+        <input type="text" name="movieName"></input>
+        {/* <input type="text" name="movieName" onChange={(e)=>{
+          setMovieName(e.target.value)
+        }}></input> */}
+
+        <label>Review : </label>
+        <input type="text" name="review"></input>
+        {/* <input type="text" name="review" onChange={(e)=>{
+          setReview(e.target.value)
+        }}></input> */}
+
+        <button >Envoi</button>
+        {/* <button onClick={submitReview}>Envoi</button> */}
+      </div>
+
     </div>
   );
 }
